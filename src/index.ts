09b8fe9e6e2da408eba622f9220ed7853c4803f9
Jose Raupp da Rosa Juniort <jose.raupp@querointernet.net>
2021-06-16 04:50:56 +0000

    updating index file and add a test basic file.

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,3 @@
 import OZMapSDK from "./OZMapSDK";
 
-(async function() {
-    const ozmap = new OZMapSDK('https://demo.ozmap.com.br');
-    await ozmap.authentication('admin', 'samplePassword');
-    let boxes = await ozmap.getBox().getAllBoxes();
-    //let boxes = await ozmap.getBox().getBoxById('60c248c1da36980022f150ec');
-    console.log(boxes);
-})();
+export default OZMapSDK;
