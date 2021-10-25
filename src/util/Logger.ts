6200f4bb652d780d853fa5ae02397787ad2f13b1
Guilherme Lima Gonçalves <guilherme.goncalves@ozmap.com.br>
2021-10-25 18:24:35 -0300

    OZMAP-3219 feature(logger): #time 20m integrate new logger implementation to the SDK

--- src/util/Logger.ts
+++ src/util/Logger.ts
@@ -1,29 +1,53 @@
 "use strict";
 import * as winston from "winston";
 
-const logger = (moduleName: string) => {
+const levels = {
+  error: 0,
+  warn: 1,
+  info: 2,
+  http: 3,
+  debug: 4,
+};
+
+const level = (): string => {
+  const env = process.env.NODE_ENV || "development";
+  const isDevelopment = env === "development";
+
+  return isDevelopment ? "debug" : "warn";
+};
+
+const colors = {
+  error: "red",
+  warn: "yellow",
+  info: "green",
+  http: "magenta",
+  debug: "white",
+};
+
+winston.addColors(colors);
+
+const logger = (moduleName: string): winston.Logger => {
+  const format = winston.format.combine(
+    winston.format.label({ label: moduleName }),
+    winston.format.colorize({ all: true }),
+    winston.format.label({ label: moduleName }),
+    winston.format.timestamp({ format: "YY-MM-DD HH:MM:SS" }),
+    winston.format.printf((info) => {
+      return `${info.timestamp} - ${info.label} - ${info.level} - ${info.message}`;
+    })
+  );
+
+  const transports = [
+    new winston.transports.Console({}),
+    //new winston.transports.File({ filename: 'logs/error.log', level: 'error' }),
+    //new winston.transports.File({ filename: 'logs/all.log' }),
+  ];
+
   return winston.createLogger({
-    format: winston.format.combine(
-      winston.format.colorize({
-        all: true,
-      }),
-      winston.format.label({
-        label: moduleName,
-      }),
-      winston.format.timestamp({
-        format: "YY-MM-DD HH:MM:SS",
-      }),
-      winston.format.printf((info) => {
-        return `${info.timestamp} - ${info.label} - ${info.level} - ${info.message}`;
-      })
-      //,winston.format.json()
-    ),
-    level: "debug",
-    transports: [
-      new winston.transports.Console({}),
-      //new winston.transports.File({ filename: 'error.log', level: 'error' }),
-      //new winston.transports.File({ filename: 'all.log' }),
-    ],
+    level: level(),
+    levels,
+    format,
+    transports,
   });
 };
 
