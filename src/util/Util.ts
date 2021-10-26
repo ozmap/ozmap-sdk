51adb8edf6a26457d78b759cf3b95134c1f475dc
Guilherme Lima Gonçalves <guilherme.goncalves@ozmap.com.br>
2021-10-26 12:11:03 -0300

    OZMAP-3219 test(util): #time 40m implement unit test for the utility module

--- src/util/Util.ts
+++ src/util/Util.ts
@@ -1,4 +1,5 @@
 import IGPS from "../interface/GPS";
+import { ConversionError } from "../exceptions/ConversionError";
 
 enum SupportedCoords {
   LAT = "lat",
@@ -8,36 +9,45 @@ enum SupportedCoords {
 }
 
 class Util {
-  static R = 6378.137; // Earth radius in kilometers
+  static PIOVER180 = 0.017453292519943295; // Math.PI / 180
 
-  static deg2rad(deg: number | undefined): number {
+  static deg2rad(deg: number | undefined, optimized = true): number {
+    // Conversion of an angular value from degrees to radians
+
     if (deg !== undefined) {
-      return deg * (Math.PI / 180);
+      return optimized ? deg * Util.PIOVER180 : deg * (Math.PI / 180);
     }
 
-    throw new Error("A valid angle in degrees must be provided!");
+    throw new ConversionError("A valid angle in degrees must be provided!");
   }
 
+  private static _getCoord = (
+    elm: IGPS,
+    coordName: SupportedCoords
+  ): number | undefined => {
+    // Helper function that extracts the coordinate component
+    // i.e., lat or lng, and then converts it into a numerical value.
+
+    if (coordName in elm) {
+      const coord = elm[coordName];
+
+      return typeof coord === "string" ? parseFloat(coord) : coord;
+    }
+
+    return undefined;
+  };
+
   private static _parseElementCoords(elementA: IGPS, elementB: IGPS) {
-    const getCoord = (
-      elm: IGPS,
-      coordName: SupportedCoords
-    ): number | undefined => {
-      if (coordName in elm) {
-        const coord = elm[coordName];
-
-        return typeof coord === "string" ? parseFloat(coord) : coord;
-      }
-
-      return undefined;
-    };
+    // Helper function that parses the incoming GPS elements in an
+    // object with the latitudes and longitude converted to radians.
 
     const lat = (elm: IGPS): number | undefined =>
-      getCoord(elm, SupportedCoords.LAT) ||
-      getCoord(elm, SupportedCoords.LATITUDE);
+      Util._getCoord(elm, SupportedCoords.LAT) ||
+      Util._getCoord(elm, SupportedCoords.LATITUDE);
+
     const lng = (elm: IGPS): number | undefined =>
-      getCoord(elm, SupportedCoords.LNG) ||
-      getCoord(elm, SupportedCoords.LONGITUDE);
+      Util._getCoord(elm, SupportedCoords.LNG) ||
+      Util._getCoord(elm, SupportedCoords.LONGITUDE);
 
     return {
       A: {
@@ -45,7 +55,7 @@ class Util {
           lat: Util.deg2rad(lat(elementA)),
         }),
         ...(lng(elementA) !== undefined && {
-          lng: Util.deg2rad(lat(elementA)),
+          lng: Util.deg2rad(lng(elementA)),
         }),
       },
       B: {
@@ -53,18 +63,18 @@ class Util {
           lat: Util.deg2rad(lat(elementB)),
         }),
         ...(lng(elementB) !== undefined && {
-          lng: Util.deg2rad(lat(elementB)),
+          lng: Util.deg2rad(lng(elementB)),
         }),
       },
     };
   }
 
-  static elementDistance(
-    elementA: IGPS,
-    elementB: IGPS,
-    distanceInMeters = false
-  ): number | void {
-    const { atan2, cos, sin, sqrt } = Math;
+  static elementDistance(elementA: IGPS, elementB: IGPS): number | undefined {
+    // Evaluates the distance (in km) between two points on the Earth's
+    // surface, assuming the Earth as a sphere with radius R ~= 6378 km.
+    // Based on the following derivations:
+    // 1. https://en.wikipedia.org/wiki/Great-circle_distance
+    // 2. https://drive.google.com/file/d/1WC-CucdSHqBbIDlqdAbUZtaFDWde3aEl/view?usp=sharing
 
     const parsedElements = Util._parseElementCoords(elementA, elementB);
 
@@ -73,17 +83,15 @@ class Util {
     const diffLat = B.lat && A.lat && B.lat - A.lat;
     const diffLng = B.lng && A.lng && B.lng - A.lng;
 
-    if (diffLat && diffLng) {
+    if (diffLat && diffLng && A.lat && B.lat) {
+      const { asin, cos, sqrt } = Math;
+
       const a =
-        sin(diffLat / 2) ** 2 +
-        cos(Util.deg2rad(A.lat)) *
-          cos(Util.deg2rad(B.lat)) *
-          sin(diffLng / 2) ** 2;
-      const c = 2 * atan2(sqrt(a), sqrt(1 - a));
+        0.5 -
+        cos(diffLat / 2) / 2 +
+        (cos(A.lat) * cos(B.lat) * (1 - cos(diffLng))) / 2;
 
-      const distance = Util.R * c; // distance in kilometers
-
-      return distanceInMeters ? distance * 1000 : distance;
+      return 12756 * asin(sqrt(a)); // 2*R = 12756 km; R = 6378 km
     }
   }
 }
