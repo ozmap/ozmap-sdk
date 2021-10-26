375cf819fb1d47601efe0161e3c2ba96b10e6e9a
Guilherme Lima Gonçalves <guilherme.goncalves@ozmap.com.br>
2021-10-26 12:07:57 -0300

    OZMAP-3219 feat(exceptions): #time 20m create custom exception structure

---
+++ src/exceptions/ConversionError.ts
@@ -0,0 +1,19 @@
+import { CustomError } from "./CustomError";
+
+class ConversionError extends CustomError {
+  statusCode = 500;
+  private static _reason = "Failed to perform conversion of value.";
+
+  constructor(message?: string) {
+    super(message || ConversionError._reason);
+
+    // Only because I'm extending a built in class.
+    Object.setPrototypeOf(this, ConversionError.prototype);
+  }
+
+  serializeErrors(): { message: string; field?: string }[] {
+    return [{ message: ConversionError._reason }];
+  }
+}
+
+export { ConversionError };
---
+++ src/exceptions/CustomError.ts
@@ -0,0 +1,14 @@
+abstract class CustomError extends Error {
+  abstract statusCode: number;
+
+  constructor(message: string) {
+    super(message);
+
+    // Only because I'm extending a built in class.
+    Object.setPrototypeOf(this, CustomError.prototype);
+  }
+
+  abstract serializeErrors(): { message: string; field?: string }[];
+}
+
+export { CustomError };
