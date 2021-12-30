0def2213ff7255ef7643119a4e0cb2e6549b7810
Guilherme Lima Goncalves <guilherme.goncalves@pland.com.br>
2021-12-29 23:09:47 -0300

    refactor: separate Jest settings into a different JS module

---
+++ jest.config.cjs
@@ -0,0 +1,35 @@
+const config = {
+  collectCoverageFrom: [
+    "src/lib/**/*.ts",
+    "src/util/Util.ts",
+    '!**/*.config.{cjs,js,ts}',
+    '!**/*.d.ts',
+    '!**/node_modules/**',
+  ],
+  coveragePathIgnorePatterns: [
+    "<rootDir>/node_modules"
+  ],
+  coverageReporters: [
+    "json",
+    "lcov",
+    "text"
+  ],
+  coverageThreshold: {
+    "global": {
+      "branches": 80,
+      "functions": 80,
+      "lines": 80,
+      "statements": 80
+    }
+  },
+  preset: "ts-jest",
+  testEnvironment: "node",
+  testRegex: "(/__tests__/.*|\\.(test|spec))\\.(ts)$",
+  moduleFileExtensions: [
+    "ts",
+    "js",
+    "json",
+  ]
+};
+
+module.exports = config;
