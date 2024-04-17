ee5fc1482258e9a1cb119b7fd4d6e65f0d5382dd
Christian Zirke <christian.zirke@clevertech.biz>
2024-04-17 12:23:51 -0300

    feat: box template example

--- src/proxy/Base.ts
+++ src/proxy/Base.ts
@@ -1,124 +1,15 @@
-import Logger from '../util/Logger';
+import { BaseModel } from '../interface/model';
 
-const logger = Logger(__filename);
+abstract class Base<Model extends BaseModel, CreateDTO, UpdateDTO> {
+  protected abstract get _route(): string;
 
-import RESTAPI from '../util/RESTAPI';
-import IPagination from '../interface/IPagination';
-import IModel from '../interface/model/IModel';
-import IFilter from '../interface/IFilter';
-import OZMapSDK from '../OZMapSDK';
-import { EnumOperator } from '../interface/EnumOperator';
-import ObjectID from 'bson-objectid';
-import IReadQueryInput from '../interface/IReadQueryInput';
-import request = require('superagent');
+  public abstract create(_model: CreateDTO): Promise<Model>;
 
-abstract class Base {
-  protected abstract endpoint: string;
-  protected restapiObject: RESTAPI;
-  protected ozmapSdk: OZMapSDK;
+  public abstract update(_model: UpdateDTO): Promise<void>;
 
-  constructor(restapi: RESTAPI, ozmapSdk: OZMapSDK) {
-    this.restapiObject = restapi;
-    this.ozmapSdk = ozmapSdk;
-  }
+  public abstract find(_ids: Array<string>): Promise<Array<Model>>;
 
-  protected get restapi(): RESTAPI {
-    if (!this.restapiObject.isConnected()) {
-      logger.error('OZMap is not connected yet. Call .authentication() with correct params, before use it');
-      throw new Error('OZMap is not connected yet. Call .authentication() before use it');
-    }
-    return this.restapiObject;
-  }
-
-  abstract create(model: IModel): Promise<IModel>;
-
-  abstract update(model: IModel): Promise<void>;
-
-  abstract getByIds(ids: Array<ObjectID>): Promise<Array<IModel>>;
-
-  abstract delete(id: ObjectID): Promise<IModel>;
-
-  abstract getById(id: ObjectID): Promise<IModel>;
-
-  abstract getAll(): Promise<IPagination<IModel>>;
-
-  abstract getAllByFilter(filter: Array<IFilter>): Promise<IPagination<IModel>>;
-
-  abstract getAllByQuery(readQueryInput: IReadQueryInput): Promise<IPagination<IModel>>;
-
-  //Helpers to get data from proxy.
-  protected async createHelper<T>(model: IModel): Promise<T> {
-    if (model.id) {
-      throw new Error('ID should not be set when creating');
-    }
-    return this.restapi.create(this.endpoint, model);
-  }
-
-  protected async deleteHelper<T>(id: IModel | ObjectID): Promise<T> {
-    if (!(id instanceof ObjectID)) {
-      id = id.id as ObjectID;
-    }
-
-    if (!id) {
-      throw new Error('ID is required for delete');
-    }
-    return this.restapi.delete<T>(this.endpoint, id as ObjectID);
-  }
-
-  protected async updateHelper(model: IModel): Promise<void> {
-    if (!model.id) {
-      throw new Error('Id is required for updates');
-    }
-    return this.restapi.update(this.endpoint, model.id, model);
-  }
-
-  protected getAllHelper<T extends IModel>(): Promise<IPagination<T>> {
-    return this.restapi.fetchAllWithPagination<T>({ model: this.endpoint });
-  }
-
-  protected getAllByFilterHelper<T extends IModel>(filter: Array<IFilter>): Promise<IPagination<T>> {
-    const readFilter: IReadQueryInput = {
-      model: this.endpoint,
-      filter: filter,
-      limit: -1,
-    };
-    return this.restapi.read<T>(readFilter);
-  }
-
-  protected getAllByQueryHelper<T extends IModel>(readQueryInput: IReadQueryInput): Promise<IPagination<T>> {
-    readQueryInput.model = this.endpoint;
-    return this.restapi.read<T>(readQueryInput);
-  }
-
-  protected getAllPaginatedByQueryHelper<T extends IModel>(readQueryInput: IReadQueryInput): Promise<IPagination<T>> {
-    readQueryInput.model = this.endpoint;
-    return this.restapi.fetchAllWithPagination<T>(readQueryInput);
-  }
-
-  protected byIdHelper<T extends IModel>(id: ObjectID | IModel): Promise<T> {
-    if (!(id instanceof ObjectID)) {
-      if (!id.id) {
-        throw new Error('ID is required for gteById');
-      }
-      id = id.id;
-    }
-    return this.restapi.readById<T>(this.endpoint, id);
-  }
-
-  protected async byIdsHelper<T extends IModel>(ids: Array<ObjectID>): Promise<Array<T>> {
-    const paginatedModels = await this.restapi.read<T>({
-      model: this.endpoint,
-      filter: [{ property: 'id', operator: EnumOperator.IN, value: ids }],
-    });
-    return paginatedModels.rows;
-  }
-
-  protected async batchUpdateHelper(filter: IFilter[], update: IModel): Promise<request.Response> {
-    const update_model = this.endpoint;
-    const v2_route = `${update_model}/batch-update`;
-    const data = { update, filter };
-    return await this.restapi.customRequest('POST', v2_route, undefined, data);
-  }
+  public abstract delete(_id: string, options: { cascade?: boolean }): Promise<void>;
 }
 
 export default Base;
