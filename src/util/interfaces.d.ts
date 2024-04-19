6085a25338d3f1ffabc6454dd481f1753ee6e509
Christian Zirke <christian.zirke@clevertech.biz>
2024-04-19 16:38:42 -0300

    feat: removing old files

--- src/util/interfaces.d.ts
+++
@@ -1,5 +0,0 @@
-interface StringParsingAPI {
-  removeSpecialChars: GenericFunction<void, StringParsingAPI>;
-  removeAccentuation: GenericFunction<void, StringParsingAPI>;
-  parsed: GenericFunction<void, string>;
-}
