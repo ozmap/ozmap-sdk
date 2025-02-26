57db4a9676201e45ed0f844c2d60ea420ad71a2b
gabrielmarioto <gabriel.marioto@ozmap.com.br>
2025-02-26 13:34:45 -0300

    OZSDK-35 feat(Role): #time 5m add new DTO to Role

--- src/interface/model/Role.ts
+++ src/interface/model/Role.ts
@@ -9,7 +9,11 @@ const RoleDataSchema = z.object({
 });
 
 const RoleSchema = BaseModelSchema.merge(RoleDataSchema);
+const CreateRoleDTOSchema = RoleDataSchema.merge(z.object({ external_id: z.any().optional() }));
+const UpdateRoleDTOSchema = RoleDataSchema.merge(z.object({ external_id: z.any().optional() })).partial();
 
 type Role = z.infer<typeof RoleSchema>;
+type CreateRoleDTO = z.infer<typeof CreateRoleDTOSchema>;
+type UpdateRoleDTO = z.infer<typeof UpdateRoleDTOSchema>;
 
-export { RoleSchema, Role };
+export { RoleSchema, Role, CreateRoleDTOSchema, CreateRoleDTO, UpdateRoleDTOSchema, UpdateRoleDTO };
